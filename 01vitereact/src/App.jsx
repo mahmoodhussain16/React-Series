1be1ad5821da39da77e8c8b1579a631738822c90
Mahmood Hussain <hussainmahmood952@gmail.com>
2024-08-27 15:46:48 +0500

    01vitereact

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Chai from "./chai"
+function App() {
+
+  return (
+    <>
+     <Chai/>
+     <h1>Mahmood Hussain</h1>
+     <p>Chai aur</p>
+    
+    </>
+   
+    
+  )
+}
+
+export default App
