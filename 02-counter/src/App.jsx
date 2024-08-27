0ab20f8630b8432a99c61bd907cadc744dfddafa
Mahmood Hussain <hussainmahmood952@gmail.com>
2024-08-27 23:38:06 +0500

    counter app

---
+++ 02-counter/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  const [counter,setCounter]=useState(0)
+  const Increment=()=>{
+    setCounter(counter+1)
+  
+  }
+ 
+  const Decrement=()=>{
+    setCounter(counter-1)
+    
+    
+   
+ 
+  }
+  const Reset=()=>{
+    setCounter(0)
+  
+  }
+
+  return (
+    <>
+     <h1>Counter App</h1>
+     <h2> {counter}</h2>
+     <button onClick={Increment}>Increment</button>
+     <button onClick={Decrement}>Decrement</button>
+     <button onClick={Reset}>Reset</button>
+    </>
+  )
+}
+
+export default App
