7181e5f2f0099fe42de47f0fca3d4d26790212b8
Mahmood Hussain <hussainmahmood952@gmail.com>
2024-08-29 21:59:41 +0500

    props concept

--- 03taiwindprops/src/App.jsx
+++ 03taiwindprops/src/App.jsx
@@ -2,13 +2,15 @@
 import './App.css'
 import Card from './components/Card/Card'
 function App() {
-  
+
+    
 
   return (
     <>
      <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind Test</h1>
-     <Card/>
-     <Card/>
+    
+     <Card username="chai aur code"btnText="visit me" />
+     <Card username="Mahmood" btnText="visit me" />
     
     
     </>
--- 03taiwindprops/src/components/Card/Card.jsx
+++ 03taiwindprops/src/components/Card/Card.jsx
@@ -1,12 +1,12 @@
 
 
-function Card() {
-  
+function Card({username,btnText}) {
+  // console.log(username);
     return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
     <img className="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains"/>
     <div className="px-6 py-4">
-      <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+      <div className="font-bold text-xl mb-2">{username}</div>
       <p className="text-gray-700 text-base">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
       </p>
@@ -15,6 +15,7 @@ function Card() {
       <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
       <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
       <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+      <button>{btnText}</button>
     </div>
   </div>
        )
