891d03753838b800fba29379a9957555026373ac
Mahmood Hussain <hussainmahmood952@gmail.com>
2024-08-29 22:20:50 +0500

    interview question

--- 02-counter/src/App.jsx
+++ 02-counter/src/App.jsx
@@ -8,14 +8,18 @@ function App() {
   const [counter,setCounter]=useState(0)
   const Increment=()=>{
     setCounter(counter+1)
+
+    // interview question
+    // setCounter(prevcounter=>prevcounter+1)
+    // setCounter(prevcounter=>prevcounter+1)
+    // setCounter(prevcounter=>prevcounter+1)
+    // setCounter(prevcounter=>prevcounter+1)
   
   }
  
   const Decrement=()=>{
     setCounter(counter-1)
-    
-    
-   
+     
  
   }
   const Reset=()=>{
